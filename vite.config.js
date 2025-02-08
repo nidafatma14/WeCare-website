53b2325843a757c5ac8d24528e5b6b4837f18d0a
Rupam Sarkar <161940237+Rupamsarkar123@users.noreply.github.com>
2025-02-08 18:05:42 +0530

    Update vite.config.js & Allowed Render to detect the open port

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+ import { defineConfig } from 'vite'
+ import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Allow Render to detect the open port
+    port: process.env.PORT || 4173, // Use Render's provided PORT
+  },
+});
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+  
+// })
